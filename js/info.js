0077cd52201001b7143ffd711139d3c7bdf7c12b
qudrn0501 <qudrn0501@naver.com>
2022-07-07 16:05:55 +0900

    swiper 시도

---
+++ js/info.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
