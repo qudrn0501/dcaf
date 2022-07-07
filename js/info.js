c29d852a0fd2d77e4cb2fc001dae8ad8fb9486d0
qudrn0501 <qudrn0501@naver.com>
2022-07-07 17:49:03 +0900

    SWIPER TESTING

--- js/info.js
+++ js/info.js
@@ -1,8 +1,7 @@
 var swiper = new Swiper(".mySwiper2", {
   slidesPerView: 1,
   spaceBetween: 30,
-  loop: true,
-  loopFillGroupWithBlank: true,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
