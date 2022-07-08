3b39ccc769e2e03ee1f3939f3a51593ec2254f00
qudrn0501 <qudrn0501@naver.com>
2022-07-08 10:35:11 +0900

    윗 페이지네이션 완성

--- js/info.js
+++ js/info.js
@@ -1,4 +1,15 @@
-var swiper = new Swiper(".mySwiper2", {
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 26.6667,
+  slidesPerGroup: 3,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 1,
   spaceBetween: 30,
 
