56675303fc412e2cf14f15fe1e22ac5644d9dff3
qudrn0501 <qudrn0501@naver.com>
2022-07-08 12:49:00 +0900

    탭메뉴 기능 완성

--- js/info.js
+++ js/info.js
@@ -1,3 +1,4 @@
+//swiper 1
 var swiper1 = new Swiper(".mySwiper1", {
   slidesPerView: 3,
   spaceBetween: 26.6667,
@@ -8,7 +9,7 @@ var swiper1 = new Swiper(".mySwiper1", {
   },
 });
 
-
+//swiper 2
 var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -18,3 +19,18 @@ var swiper2 = new Swiper(".mySwiper2", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//tab menu
+function openTab(evt, tabName) {
+  var i, tabContent, tabLinks;
+  tabContent = document.getElementsByClassName("tab_content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+  tabLinks = document.getElementsByClassName("tab_links");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
