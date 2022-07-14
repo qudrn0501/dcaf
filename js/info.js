4e0cf79f2b9df353da09fb6d56c3875f59fb46f3
qudrn0501 <qudrn0501@naver.com>
2022-07-14 09:26:54 +0900

    0714

--- js/info.js
+++ js/info.js
@@ -11,9 +11,9 @@ var swiper1 = new Swiper(".mySwiper1", {
 
 //swiper 3
 var swiper3 = new Swiper(".mySwiper3", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
-  slidesPerGroup: 4,
+  slidesPerGroup: 1,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -31,17 +31,27 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 
-//tab menu
-function openTab(evt, tabName) {
-  var i, tabContent, tabLinks;
-  tabContent = document.getElementsByClassName("tab_content");
-  for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = "none";
-  }
-  tabLinks = document.getElementsByClassName("tab_links");
-  for (i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+// //tab menu
+// function openTab(evt, tabName) {
+//   var i, tabContent, tabLinks;
+//   tabContent = document.getElementsByClassName("tab_content");
+//   for (i = 0; i < tabContent.length; i++) {
+//     tabContent[i].style.display = "none";
+//   }
+//   tabLinks = document.getElementsByClassName("tab_links");
+//   for (i = 0; i < tabLinks.length; i++) {
+//     tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(tabName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+$(document).ready(function () {
+  $(".tab_links").click(function () {
+    var idx = $(this).index();
+    $(".tab_links").removeClass("on_active");
+    $(".tab_links").eq(idx).addClass("on_active");
+    $(".tab_content > div").hide();
+    $(".tab_content > div").eq(idx).show();
+  })
+});
